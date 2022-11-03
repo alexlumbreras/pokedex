f180c16b4e26b63e6710efaf43ddc2a47ff1d4a8
Alejandro Lumbreras <alex.lumlo@gmail.com>
2022-11-03 16:56:15 +0100

    Refactorize App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import App from './App';
 
 test('renders react app', () => {
   render(<App />);
-  const header = screen.getByText(/my pokedex/i);
-  expect(header).toBeInTheDocument();
+  const heading = screen.getByRole('heading', { level: 1, name: /my pokedex/i });
+  expect(heading).toBeInTheDocument();
 });
 
