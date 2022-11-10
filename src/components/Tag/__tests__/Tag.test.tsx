50794b2c530f896c86de3a93d8ae2399d49c1b7a
Alejandro Lumbreras <alex.lumlo@gmail.com>
2022-11-10 19:31:17 +0100

    Test[red]: Renders a ghost type component.

--- src/components/Tag/__tests__/Tag.test.tsx
+++ src/components/Tag/__tests__/Tag.test.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { screen, render } from "@testing-library/react";
 import { Tag } from "../Tag";
 
-test('Tag (grass type) component is renderized', () => {
-    render(<Tag color="grass">Grass</Tag>);
+test('Tag (ghost type) component is renderized', () => {
+    render(<Tag color="ghost">Ghost</Tag>);
 
-    const tag = screen.getByText(/grass/i);
+    const tag = screen.getByText(/ghost/i);
 
     expect(tag).toBeInTheDocument();
     // expect(tag).toHaveStyle({backgroundColor: 'var(--type-ground)'})
