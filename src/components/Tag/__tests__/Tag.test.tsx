15551d642e8fd395f3b7ba442137f2c7f39c6314
Alejandro Lumbreras <alex.lumlo@gmail.com>
2022-11-15 10:06:39 +0100

    [Fix]: Cambio de unos atributos de la etiqueta svg que provocaban un warning en el test + modificación del Tag.test.tsx.

--- src/components/Tag/__tests__/Tag.test.tsx
+++ src/components/Tag/__tests__/Tag.test.tsx
@@ -1,12 +1,13 @@
-import React from "react";
 import { screen, render } from "@testing-library/react";
 import { Tag } from "../Tag";
 
-test("Tag (ghost type) component is renderized", () => {
-  render(<Tag color="ghost">Ghost</Tag>);
+describe("Tag", () => {
+  it("Tag (ghost type) component is renderized", () => {
+    render(<Tag color="grass">grass</Tag>);
 
-  const tag = screen.getByText(/ghost/i);
+    const tag = screen.getByText(/grass/i);
 
-  expect(tag).toBeInTheDocument();
-  // expect(tag).toHaveStyle({backgroundColor: 'var(--type-ground)'})
+    expect(tag).toBeInTheDocument();
+    expect(tag).toHaveStyle({ backgroundColor: "var(--type-grass)" });
+  });
 });
