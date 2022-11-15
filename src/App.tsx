673145806c2eb7ea96f656ebb9add11cd490f58a
Alejandro Lumbreras <alex.lumlo@gmail.com>
2022-11-15 13:32:40 +0100

    Creación del componente PokemonCard con algunos de los props necesarios como name, index e imgUrl.

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,27 @@ import { Icon } from "./components/Icon/Icon";
 import { Tag } from "./components/Tag";
 import { Pokeball } from "./components/Icon/icons/Pokeball";
 import { SearchIcon } from "./components/Icon/icons/SearchIcon";
+import { PokemonCard } from "components/PokemonCard";
 
 function App() {
+  const cardData = {
+    index: 1,
+    name: "Bulbasur",
+    imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+    types: ["grass"],
+    weight: 6.9,
+    height: 0.7,
+    description:
+      "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+  };
+
   return (
     <main>
       <h1>My Pokedex</h1>
       <Tag color="grass">grass</Tag>
       <Icon icon={Pokeball} />
       <Icon icon={SearchIcon} />
+      <PokemonCard {...cardData}></PokemonCard>
     </main>
   );
 }
---
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -0,0 +1,79 @@
+import { HeightIcon } from "components/Icon/icons/HeightIcon";
+import { WeightIcon } from "components/Icon/icons/WeightIcon";
+import { Tag } from "components/Tag/Tag";
+import styles from "./PokemonCard.module.css";
+
+type CardProps = {
+  index: number;
+  name: string;
+  imageUrl: string;
+  types: string[];
+  weight: number;
+  height: number;
+  description: string;
+};
+
+export const PokemonCard: React.FC<CardProps> = ({
+  index,
+  name,
+  imageUrl,
+  types,
+  weight,
+  height,
+  description,
+}) => {
+  const formattedIndex = `#${index.toString().padStart(3, "0")}`;
+
+  types.map((type) => console.log(type));
+
+  return (
+    <div className={styles.cardWrapper}>
+      <div className={styles.header}>
+        <h4 className={styles.name}>{name}</h4>
+        <p className={styles.index}>{formattedIndex}</p>
+      </div>
+
+      <div className={styles.content}>
+        <img className={styles.image} src={imageUrl} alt={name} />
+        <div className={styles.information}>
+          <div className={styles.types}>
+            {types.map((type: string, tagIndex: number) => (
+              <Tag key={`card-type-${tagIndex}`} color={type}>
+                {type}
+              </Tag>
+            ))}
+          </div>
+
+          <p
+            className={styles.about}
+            style={{
+              color: `var(--type-${types[0]}`,
+            }}
+          >
+            About
+          </p>
+
+          <div className={styles.measures}>
+            <div className={styles.measure}>
+              <div className={styles.measureData}>
+                <WeightIcon color={`var(--dark-gray)`} />
+                {`${weight} kg`}
+              </div>
+              <p className={styles.measureName}>Weight</p>
+            </div>
+
+            <div className={styles.measure}>
+              <div className={styles.measureData}>
+                <HeightIcon color={`var(--dark-gray)`} />
+                {`${height} m`}
+              </div>
+              <p className={styles.measureName}>Height</p>
+            </div>
+          </div>
+
+          <p className={styles.description}>{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
