20c0891d11701011a7e0cba6b6f98828df509bca
Alejandro Lumbreras <alex.lumlo@gmail.com>
2022-11-24 15:03:14 +0100

    [Refactor] Delete SearchIcon component and import search logo svg

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,9 +1,9 @@
 import styles from "../SearchBar/SearchBar.module.css";
-import { SearchIcon } from "components/Icon/icons/SearchIcon";
+import SearchIcon from "../../assets/images/Search-icon.svg";
 
 export const SearchBar = () => (
   <div className={styles.searcherWrapper}>
-    <SearchIcon color={"var(--medium-gray)"} />
+    <img src={SearchIcon} alt="Search-Icon" />
     <input className={styles.searcher}></input>
   </div>
 );
