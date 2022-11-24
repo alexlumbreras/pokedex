12f85489daaf6a2b6e9bf08592db470404d499c1
Alejandro Lumbreras <alex.lumlo@gmail.com>
2022-11-24 13:45:14 +0100

    [Refactor] Remove PokeballIcon component and its test
    Remove PokeballIcon component and its test beacause we delete it pevoiously.

--- src/components/Icon/__tests__/Icon.test.tsx
+++ src/components/Icon/__tests__/Icon.test.tsx
@@ -1,16 +1,8 @@
 import { screen, render } from "@testing-library/react";
 import { Icon } from "../Icon";
-import { Pokeball } from "../icons/Pokeball";
 import { SearchIcon } from "../icons/SearchIcon";
 
 describe("Icon", () => {
-  it("Renders Pokeball Icon", () => {
-    render(<Icon icon={Pokeball} />);
-
-    const pokeball = screen.getByRole("img");
-
-    expect(pokeball).toHaveAttribute("name", "pokeball-icon");
-  });
   it("Renders Search Icon", () => {
     render(<Icon icon={SearchIcon} />);
 
