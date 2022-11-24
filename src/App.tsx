76a7c5955c5c60152a683c819820e5a7ab177ba9
Alejandro Lumbreras <alex.lumlo@gmail.com>
2022-11-24 12:04:54 +0100

    [Refactor] Remove components from main App
    Remove Tag, Pokeball and SearchIcon to remain only necessary components on main App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-import { Icon } from "./components/Icon/Icon";
-import { Tag } from "./components/Tag";
-import { Pokeball } from "./components/Icon/icons/Pokeball";
-import { SearchIcon } from "./components/Icon/icons/SearchIcon";
 import { PokemonCard } from "components/PokemonCard";
 
 function App() {
@@ -20,9 +16,6 @@ function App() {
   return (
     <main>
       <h1>My Pokedex</h1>
-      <Tag color="grass">grass</Tag>
-      <Icon icon={Pokeball} />
-      <Icon icon={SearchIcon} />
       <PokemonCard {...cardData}></PokemonCard>
     </main>
   );
