c60409cc0eec3642a474c6602a51884fd948af4f
Alejandro Lumbreras <alex.lumlo@gmail.com>
2022-11-24 13:20:05 +0100

    [Test] Test renderization of Header component.
    Test the renderization of heading and pokeball logo..

--- src/components/Header/__tests__/Header.test.tsx
+++ src/components/Header/__tests__/Header.test.tsx
@@ -1,1 +1,23 @@
+import { render, screen } from "@testing-library/react";
+import { Header } from "../Header";
 
+describe("Render Header component", () => {
+  test("Render header title", () => {
+    render(<Header />);
+
+    const heading = screen.getByRole("heading", {
+      level: 1,
+      name: /my pokedex/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+
+  test("Render header logo", () => {
+    render(<Header />);
+
+    const pokeballIcon = screen.getByAltText(/pokeball-icon/i);
+
+    expect(pokeballIcon).toBeInTheDocument();
+  });
+});
