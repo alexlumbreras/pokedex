95419c38288c85a7351e840bcf0b85913077472d
Alejandro Lumbreras <alex.lumlo@gmail.com>
2022-11-28 13:50:28 +0100

    [Test] Test Footer component

--- src/components/Footer/__tests__/Footer.test.tsx
+++ src/components/Footer/__tests__/Footer.test.tsx
@@ -2,7 +2,13 @@ import { render, screen } from "@testing-library/react";
 import { Footer } from "../Footer";
 
 describe("Footer", () => {
-  test("Reder the component", () => {
+  test("Reder Footer component", () => {
     render(<Footer />);
+
+    const footerGitImage = screen.getByAltText("Github Logo");
+    const footerApiImage = screen.getByAltText("PokeApi Logo");
+
+    expect(footerGitImage).toBeInTheDocument();
+    expect(footerApiImage).toBeInTheDocument();
   });
 });
